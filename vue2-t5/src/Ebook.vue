cde3b843d08e3e8c632fb963ebc1442b40604d6f
lmy <rain_lm@outlook.com>
2018-09-10 10:34:22 +0800

    进度条

--- vue2-t5/src/Ebook.vue
+++ vue2-t5/src/Ebook.vue
@@ -10,15 +10,9 @@
                 <div class="right" @click="nextPage"></div>
             </div>
         </div>
-        <menu-bar 
-        :ifTitleAndMenuShow = "ifTitleAndMenuShow"
-        :fontSizeList = "fontSizeList"
-        :defaultFontSize = "defaultFontSize"
-        @setFontSize = "setFontSize"
-				:themeList = "themeList"
-				:defaultTheme = "defaultTheme"
-				@setTheme = "setTheme"
-        ref = "menuBar"></menu-bar>
+        <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" :fontSizeList="fontSizeList" :defaultFontSize="defaultFontSize" @setFontSize="setFontSize" :themeList="themeList" :defaultTheme="defaultTheme" @setTheme="setTheme"
+        :bookAvailable="bookAvailable"
+        @onProgressChange="onProgressChange" ref="menuBar"></menu-bar>
     </div>
 </template>
 
@@ -32,80 +26,87 @@ export default {
     return {
       ifTitleAndMenuShow: false,
       fontSizeList: [
-          { fontSize: 12 },
-          { fontSize: 14 },
-          { fontSize: 16 },
-          { fontSize: 18 },
-          { fontSize: 20 },
-          { fontSize: 22 },
-          { fontSize: 24 }
+        { fontSize: 12 },
+        { fontSize: 14 },
+        { fontSize: 16 },
+        { fontSize: 18 },
+        { fontSize: 20 },
+        { fontSize: 22 },
+        { fontSize: 24 }
       ],
       defaultFontSize: 16,
       themeList: [
-          {
-              name: 'default',
-              style: {
-                  body: {
-                      'color': '#000', 
-                      'background': '#fff'
-                  }
-              }
-          },
-          {
-              name: 'eye',
-              style: {
-                  body: {
-                      'color': '#000', 
-                      'background': '#ceeaba'
-                  }
-              }
-          },
-          {
-              name: 'night',
-              style: {
-                  body: {
-                      'color': '#fff', 
-                      'background': '#000'
-                  }
-              }
-          },
-          {
-              name: 'gold',
-              style: {
-                  body: {
-                      'color': '#fff', 
-                      'background': 'rgb(241, 236, 226)'
-                  }
-              }
+        {
+          name: 'default',
+          style: {
+            body: {
+              color: '#000',
+              background: '#fff'
+            }
           }
-			],
-			defaultTheme: 0
+        },
+        {
+          name: 'eye',
+          style: {
+            body: {
+              color: '#000',
+              background: '#ceeaba'
+            }
+          }
+        },
+        {
+          name: 'night',
+          style: {
+            body: {
+              color: '#fff',
+              background: '#000'
+            }
+          }
+        },
+        {
+          name: 'gold',
+          style: {
+            body: {
+              color: '#fff',
+              background: 'rgb(241, 236, 226)'
+            }
+          }
+        }
+      ],
+      defaultTheme: 0,
+      bookAvailable: false
     }
   },
   components: {
-      TitleBar,
-      MenuBar 
+    TitleBar,
+    MenuBar
   },
   methods: {
-		setTheme(index) {
-			this.defaultTheme = index
-			this.themes.select(this.themeList[index].name)
-		},
+    onProgressChange(progress) {
+      const percentage = progress / 100
+      const location =
+        percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
+      this.rendition.display(location)
+    },
+    setTheme(index) {
+      this.defaultTheme = index
+      this.themes.select(this.themeList[index].name)
+    },
     registerTheme() {
       this.themeList.forEach(theme => {
-				this.themes.register(theme.name, theme.style)
-			})
+        this.themes.register(theme.name, theme.style)
+      })
     },
     setFontSize(fontSize) {
-        this.defaultFontSize = fontSize
-        if(this.themes) {
-            this.themes.fontSize(fontSize + 'px')
-        }
+      this.defaultFontSize = fontSize
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
+      }
     },
     toggleTitleAndMenu() {
       this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
-      if(!this.ifTitleAndMenuShow) {
-          this.$refs.menuBar.hideSetting()
+      if (!this.ifTitleAndMenuShow) {
+        this.$refs.menuBar.hideSetting()
       }
     },
     prevPage() {
@@ -129,14 +130,17 @@ export default {
       // 获取theme对象
       this.themes = this.rendition.themes
       this.setFontSize(this.defaultFontSize)
-			
-			this.registerTheme()
-			this.themes.select(this.defaultTheme)
-			this.book.ready.then(()=>{
-				this.book.locations.generate()
-			}).then(result=>{
-				console.log(result)
-			})
+
+      this.registerTheme()
+      this.themes.select(this.defaultTheme)
+      this.book.ready
+        .then(() => {
+          return this.book.locations.generate()
+        })
+        .then(result => {
+          this.locations = this.book.locations
+          this.bookAvailable = true
+        })
     }
   },
   mounted() {
--- vue2-t5/src/components/MenuBar.vue
+++ vue2-t5/src/components/MenuBar.vue
@@ -8,7 +8,7 @@
 					<span class="icon-menu icon"></span>
 				</div>
 				<div class="icon-wrapper">
-					<span class="icon-progress icon"></span>
+					<span class="icon-progress icon" @click="showSetting(2)"></span>
 				</div>
 				<div class="icon-wrapper">
 					<span class="icon-bright icon" @click="showSetting(1)"></span>
@@ -44,6 +44,22 @@
 						<div class="text" :class="{'selected': index === defaultTheme}">{{item.name}}</div>
 					</div>
 				</div>
+				<div class="setting-progress" v-else-if="showTag === 2">
+					<div class="progress-wrapper">
+						<input class="progress" type="range"
+						max="100"
+						min="0"
+						step="1"
+						@change="onProgressChange($event.target.value)"
+						@input="onProgressInput($event.target.value)"
+						:value="0"
+						:disabled="!bookAvailable"
+						ref="progress">
+					</div>
+					<div class="text-wrapper">
+						<span>{{bookAvailable ? progress + '%' : '加载中...'}}</span>
+					</div>
+				</div>
 			</div>
 		</transition>
 	</div>
@@ -66,15 +82,27 @@ export default {
 		defaultTheme: {
 			type: Number
 		},
-		themeList: Array
+		themeList: Array,
+		bookAvailable: {
+			type: Boolean,
+			default: false
+		}
   },
   data() {
     return {
 			ifSettingShow: false,
-			showTag: 0
+			showTag: 0,
+			progress: 0
     }
   },
   methods: {
+		onProgressInput(progress){
+			this.progress = progress
+			this.$refs.progress.style.backgroundSize = `${this.progress}% 100%`
+		},
+		onProgressChange(progress){
+			this.$emit('onProgressChange', progress)
+		},
 		setTheme(index) {
 			this.$emit('setTheme', index)
 			console.log(this.defaultTheme)
@@ -191,6 +219,41 @@ export default {
 						color: #333
 					}
 				}
+			}
+		}
+		.setting-progress{
+			position: relative;
+			width: 100%;
+			height: 100%;
+			.progress-wrapper{
+				width: 100%;
+				height: 100%;
+				@include center;
+				padding: 0 px2rem(30);
+				box-sizing: border-box;
+				.progress{
+					width: 100%;
+					-webkit-appearance: none;
+					height: px2rem(2);
+					background: -webkit-linear-gradient(#999, #999) no-repeat, #ddd;
+					background-size: 0 100%;
+					&:focus{
+						outline: none;
+					}
+					&::-webkit-slider-thumb{
+						-webkit-appearance: none;
+						height: px2rem(20);
+						width: px2rem(20);
+						border-radius: 50%;
+						background-color: white;
+						box-shadow: 0 4px 4px rgba($color: #000000, $alpha: 0.15);
+						border: px2rem(1) solid #ddd;
+					}
+				}
+			}
+			.text-wrapper{
+				font-size: px2rem(12);
+				@include center
 			}
 		}
   }
